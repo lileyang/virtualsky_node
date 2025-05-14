a09e0aa7b8a5a2b17c6d51503038d11ff9a98b1c
李乐洋 <13864172818@139.com>
2025-05-14 17:09:34 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// 一个简单的 GET 路由
+app.get('/', (req, res) => {
+  res.send('Hello from Node.js on Render!');
+});
+
+// 启动服务器
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});

36824ee8960b1da08ef83947d50973be2d38365b
李乐洋 <13864172818@139.com>
2025-05-14 17:34:00 +0800

    version v1.0

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3060;
 
 // 一个简单的 GET 路由
 app.get('/', (req, res) => {
